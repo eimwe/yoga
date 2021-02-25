e17ea4a1593faaeb556c46c55553666ddcb961bd
eimwe <prokopenkoserge@inbox.ru>
2021-02-25 23:23:50 +0600

    Third screen is done for 320px mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 window.document.addEventListener('DOMContentLoaded', function() {
 
+  //Add box-shadow to the fixed header on scroll
   window.addEventListener('scroll', function(){
     let headerNode = window.document.getElementsByTagName('HEADER')[0];
     if(window.pageYOffset > 0) {
@@ -8,5 +9,20 @@ window.document.addEventListener('DOMContentLoaded', function() {
       headerNode.classList.remove('header-fixed');
     }
   });
+
+  //Switch between .member-cards-toggle list items
+  function activeClassChanger(){
+    let liNodes = document.querySelectorAll('.member-cards-toggle li');
+
+    for (let cnt = 0; cnt < liNodes.length; cnt++) {
+      liNodes[cnt].addEventListener('click', function() {
+        let current = document.getElementsByClassName('toggle-active');
+        current[0].className = current[0].className.replace('toggle-active','');
+        this.className += "toggle-active";
+      });
+    }
+  }
+
+  activeClassChanger();
   
 }, false);
