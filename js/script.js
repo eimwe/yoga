0204e4eb1f8c6a5b7d62a569b4d96eedd059f2af
eimwe <prokopenkoserge@inbox.ru>
2021-03-04 21:54:34 +0600

    Coaches section with slider is ready for mobile version

--- js/script.js
+++ js/script.js
@@ -10,19 +10,114 @@ window.document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  //Switch between .member-cards-toggle list items
-  function activeClassChanger(){
-    let liNodes = document.querySelectorAll('.member-cards-toggle li');
 
+  //Switch between .member-cards-toggle list items
+  (function(){
+    let liNodes = document.querySelectorAll('.member-cards-toggle li'),
+        activeLiNode = document.getElementsByClassName('toggle-active');
     for (let cnt = 0; cnt < liNodes.length; cnt++) {
       liNodes[cnt].addEventListener('click', function() {
-        let current = document.getElementsByClassName('toggle-active');
-        current[0].className = current[0].className.replace('toggle-active','');
-        this.className += "toggle-active";
+        activeLiNode[0].classList.remove("toggle-active");
+        liNodes[cnt].classList.toggle("toggle-active");
       });
+    }
+  })();
+
+
+  //Coaches section slider
+  let buttonNodes = document.querySelectorAll('.coaches-slider-controls button'),
+      sliderCounterNode = document.querySelector('.slider-counter'),
+      currentSlideIndexNode = document.querySelector('.current-slide'),
+      nextSlideIndexNode = document.querySelector('.remaining-slide'),
+      descSlides = document.querySelectorAll('.coaches-desc'),
+      bioSlides = document.querySelectorAll('.coaches-bio'),
+      defaultSlide = 1,
+      currentIndex = document.createTextNode(defaultSlide),
+      remainingIndex = document.createTextNode(descSlides.length);
+
+  if(descSlides.length === bioSlides.length) {
+    currentSlideIndexNode.appendChild(currentIndex);
+    nextSlideIndexNode.appendChild(remainingIndex);
+  } else if(descSlides.length > bioSlides.length) {
+    sliderCounterNode.style.display = 'none';
+    console.error("There are more '.coaches-desc' elements than '.coaches-bio'!");
+  } else if(descSlides.length < bioSlides.length) {
+    sliderCounterNode.style.display = 'none';
+    console.error("There are more '.coaches-bio' elements than '.coaches-desc'!");
+  }
+
+  function showNextSlide(index) {
+    let currentDescSlide = document.querySelector('.coaches-slide .slide-active'),
+        currentBioSlide = document.querySelector('.coaches-bio-slide .slide-active');
+
+    if(!(currentIndex.nodeValue === remainingIndex.nodeValue)) {
+      currentIndex.nodeValue = '';
+      currentIndex = document.createTextNode(defaultSlide += index);
+      currentSlideIndexNode.appendChild(currentIndex);
+    } else {
+      defaultSlide = 1;
+      currentIndex.nodeValue = '';
+      currentIndex.nodeValue = defaultSlide;
+      currentSlideIndexNode.appendChild(currentIndex);
+    }
+    
+    if (currentDescSlide.nextElementSibling) {
+      currentDescSlide.classList.replace('slide-active', 'slide-inactive');
+      currentDescSlide.nextElementSibling.classList.replace('slide-inactive', 'slide-active');
+    } else {
+      currentDescSlide.parentElement.firstElementChild.classList.replace('slide-inactive', 'slide-active');
+      currentDescSlide.parentElement.lastElementChild.classList.replace('slide-active', 'slide-inactive');
+    }
+
+    if (currentBioSlide.nextElementSibling) {
+      currentBioSlide.classList.replace('slide-active', 'slide-inactive');
+      currentBioSlide.nextElementSibling.classList.replace('slide-inactive', 'slide-active');
+    } else {
+      currentBioSlide.parentElement.firstElementChild.classList.replace('slide-inactive', 'slide-active');
+      currentBioSlide.parentElement.lastElementChild.classList.replace('slide-active', 'slide-inactive');
     }
   }
 
-  activeClassChanger();
-  
+  function showPrevSlide(index) {
+    let currentDescSlide = document.querySelector('.coaches-slide .slide-active'),
+        currentBioSlide = document.querySelector('.coaches-bio-slide .slide-active');
+    
+    if(currentIndex.nodeValue > 1) {
+      currentIndex.nodeValue = '';
+      currentIndex = document.createTextNode(defaultSlide -= index);
+      currentSlideIndexNode.appendChild(currentIndex);
+    } else {
+      defaultSlide = remainingIndex.nodeValue;
+      currentIndex.nodeValue = '';
+      currentIndex.nodeValue = defaultSlide;
+      currentSlideIndexNode.appendChild(currentIndex);
+    }
+    
+    if (currentDescSlide.previousElementSibling) {
+      currentDescSlide.classList.replace('slide-active', 'slide-inactive');
+      currentDescSlide.previousElementSibling.classList.replace('slide-inactive', 'slide-active');
+    } else {
+      currentDescSlide.parentElement.lastElementChild.classList.replace('slide-inactive', 'slide-active');
+      currentDescSlide.parentElement.firstElementChild.classList.replace('slide-active', 'slide-inactive');
+    }
+
+    if (currentBioSlide.previousElementSibling) {
+      currentBioSlide.classList.replace('slide-active', 'slide-inactive');
+      currentBioSlide.previousElementSibling.classList.replace('slide-inactive', 'slide-active');
+    } else {
+      currentBioSlide.parentElement.lastElementChild.classList.replace('slide-inactive', 'slide-active');
+      currentBioSlide.parentElement.firstElementChild.classList.replace('slide-active', 'slide-inactive');
+    }
+  }
+
+  for(let cnt = 0; cnt < buttonNodes.length; cnt++) {
+    buttonNodes[cnt].addEventListener('click', function(){
+      if(buttonNodes[cnt].classList.contains('next-slide')) {
+        showNextSlide(1);
+      } else {
+        showPrevSlide(1);
+      }
+    });
+  }
+
 }, false);
